31437c8b852ad698f771a1e15af75137cc23ff65
Seth Brown <sl2004brown@gmail.com>
2020-08-26 18:38:19 -0600

    Make code a whole lot cleaner by using async and await instead of .then

--- Bot/rewrite.js
+++ Bot/rewrite.js
@@ -1,4 +1,4 @@
-//Import stuff so that this works I guess
+// Import stuff so that this works I guess
 const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
@@ -7,7 +7,7 @@ const { exec } = require('child_process');
 const { send, stderr, stdout } = require('process');
 const { Console } = require('console');
 const { resolve } = require('path');
-//Make the variable to manipulate
+// Make the variable to manipulate
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const prefix = config.PREFIX
 const second = 1000;
@@ -22,48 +22,42 @@ let bedrock_console = null; // The BDS process
 let chatbot_console = null;
 let grace_stop = true; // ?
 const commands = {
-    server: args => new Promise((resolve, reject) => {
+    server: async args => {
         // Test for power to the server hardware.
         if (args[0] === 'power' || args[0] === 'running') {
-            bds_running().then(running => {
-                if (running) {
-                    resolve('the server is currently running.');
-                } else {
-                    resolve('the server is not currently running.');
-                }
-            });
+            if (await bds_running()) {
+                return 'the server is currently running.';
+            } else {
+                return 'the server is not currently running.';
+            }
         } else if (args[0] === 'start') {
             // Check if it's already running or off
-            bds_running().then(running => {
-                if (running) {
-                    resolve('the server is already on.');
-                } else {
-                    ping().then(server_on => {
-                        if (!server_on) {
-                            resolve('the server is not powered on.')
-                        } else { // Otherwise, start it
-                            bedrock_console = spawn(`ssh`, [`jackson@192.168.1.7`, `"c:/Users/Jackson/Desktop/Minecraft_Server/Survival/bedrock_server.exe"`]);
-                            console.log('Starting server');
-                            resolve('the server is now running.');
-                        }
-                    })
+            if (await bds_running()) {
+                return 'the server is already on.';
+            } else {
+                if (!(await ping())) {
+                    return 'the server is not powered on.';
+                } else { // Otherwise, start it
+                    bedrock_console = spawn(`ssh`, [`jackson@192.168.1.7`, `"c:/Users/Jackson/Desktop/Minecraft_Server/Survival/bedrock_server.exe"`]);
+                    console.log('Starting server');
+                    return 'the server is now running.';
                 }
-            });     
+            }
         } else if (args[0] === 'stop') {
             bedrock_console.kill();
             bedrock_console = null;
             console.log('Killing server');
-            resolve('server terminated.');
+            return 'server terminated.';
         }
-    }),
+    },
 
-    bot: args => new Promise((resolve, reject) => {
-        resolve('this is a message back to the user');
-    })
+    bot: async args => {
+        return 'this is a message back to the user';
+    }
 };
 
 // Return to this code when a message is sent
-client.on('message', message => {
+client.on('message', async message => {
     // console.log(message);
     if (message.author.bot) return;
     
@@ -92,11 +86,10 @@ client.on('message', message => {
     if (isCommand) {
         console.log(command);
         if (commands.hasOwnProperty(command)) {
-            commands[command](args).then(response => {
-                if (response) {
-                    message.reply(response);
-                }
-            });
+            const response = await commands[command](args);
+            if (response) {
+                message.reply(response);
+            }
         } else {
             message.reply('that isn\'t a command, silly.');
         }
@@ -108,30 +101,25 @@ client.on('message', message => {
 
 // Test for connection to the server. If no connection, set st variable to null etc. If there is a connection that isn't
 // expected, kill the bedrock server and start it again to maintain a connection that it manipulatable.
-setInterval(() => {
+setInterval(async () => {
     if (bedrock_console !== null) {
-        ping().then(running => {
-            if (!running) {
+        const [computer_on, tasks_running] = await Promise.all(ping(), tasklist());
+        if (!computer_on) {
+            bedrock_console.kill();
+            bedrock_console = null;
+            console.log('The computer is not on, killing the SSH BDS process in Node');
+        } else {
+            if (!tasks_running.includes('bedrock_server.exe')) {
                 bedrock_console.kill();
-                bedrock_console = null;
-                console.log('The computer is not on, killing the SSH BDS process in Node');
-            } else {
-                tasklist().then(tasks => {
-                    if (!tasks.includes('bedrock_server.exe')) {
-                        bedrock_console.kill();
-                        bedrock_console = spawn(`ssh`, [`jackson@192.168.1.7`, `"c:/Users/Jackson/Desktop/Minecraft_Server/Survival/bedrock_server.exe"`]);
-                        console.log('Supposedly BDS is running over SSH, but it isn\'t running on the computer\nRestarting BDS over SSH');
-                    }
-                });
+                bedrock_console = spawn(`ssh`, [`jackson@192.168.1.7`, `"c:/Users/Jackson/Desktop/Minecraft_Server/Survival/bedrock_server.exe"`]);
+                console.log('Supposedly BDS is running over SSH, but it isn\'t running on the computer\nRestarting BDS over SSH');
             }
-        });
+        }
     } else {
-        tasklist().then(tasks => {
-            if (tasks.includes('bedrock_server.exe')) {
-                exec(`ssh jackson@192.168.1.7 taskkill /IM "bedrock_server.exe" /F`);
-                console.log('The BDS SSH handle that Node owns is dead, but BDS is running on the computer. Killing BDS');
-            }
-        });
+        if ((await tasklist()).includes('bedrock_server.exe')) {
+            exec(`ssh jackson@192.168.1.7 taskkill /IM "bedrock_server.exe" /F`);
+            console.log('The BDS SSH handle that Node owns is dead, but BDS is running on the computer. Killing BDS');
+        }
     }
 }, 30 * second);
 
@@ -156,25 +144,16 @@ function ping() {
 
 // List tasks running on Jackson's computer
 function tasklist() {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         exec(`ssh jackson@192.168.1.7 tasklist`, (error, stdout, stderr) => {
             resolve(stdout);
         });
     });
 }
 
-function bds_running() {
-    return new Promise((resolve, reject) => {
-        ping().then(computer_on => {
-            tasklist().then(tasks => {
-                if (computer_on && tasks.includes('bedrock_server.exe')) {
-                    resolve(true);
-                } else {
-                    resolve(false);
-                }
-            });
-        });
-    });
+async function bds_running() {
+    const [computer_on, tasks_running] = await Promise.all([ping(), tasklist()]);
+    return computer_on && tasks_running.includes('bedrock_server.exe');
 }
 
 client.login(config.BOT_TOKEN);
