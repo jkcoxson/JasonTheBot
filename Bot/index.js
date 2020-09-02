d012beaed7eb56458ca05400b4d2dfc99f802dd4
Seth Brown <sl2004brown@gmail.com>
2020-09-01 22:44:55 -0600

    Ugh

--- Bot/index.js
+++ Bot/index.js
@@ -31,7 +31,7 @@ fs.readFile('random.txt', 'utf8' , (err, data) => {
 // Global fun
 const bedrock_server = require('./server.js').bedrock_server;
 
-bedrock_server.addListener('start', () => {
+bedrock_server.on('start', () => {
     chatbot_console = spawn(`/usr/local/go/bin/go`,[`run`,`/home/open/Documents/Go/main.go`]);
 });
 
