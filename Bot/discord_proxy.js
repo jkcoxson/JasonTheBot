2a6cfdf342412a4fdb91f3cae47d7c49188a5838
Seth Brown <sl2004brown@gmail.com>
2020-09-12 16:47:21 -0600

    Get rid of death messages in discord

--- Bot/discord_proxy.js
+++ Bot/discord_proxy.js
@@ -1,39 +1,4 @@
 const channels = require('./configs/channel-ids.json');
-const loadlines = require('./loadlines.js');
-
-const death_messages = {};
-(async function() {  
-    [death_messages['arrow'], death_messages['cactus'], death_messages['creeper'],
-        death_messages['drown'], death_messages['drowned'],
-        death_messages['ender_dragon'], death_messages['ender_dragon_magic'],
-        death_messages['enderman'], death_messages['fall'],
-        death_messages['lava'], death_messages['llama'], death_messages['shulker'],
-        death_messages['spider'], death_messages['suffocate'],
-        death_messages['tnt'], death_messages['fallvoid'],
-        death_messages['witch_magic'], death_messages['wolf'],
-        death_messages['zombie']] =
-        await Promise.all([
-            loadlines('DeathMessages/Arrow.txt'),
-            loadlines('DeathMessages/Cactus.txt'),
-            loadlines('DeathMessages/Creeper.txt'),
-            loadlines('DeathMessages/Drown.txt'),
-            loadlines('DeathMessages/Drowned.txt'),
-            loadlines('DeathMessages/Ender_Dragon.txt'),
-            loadlines('DeathMessages/Ender_Dragon_Magic.txt'),
-            loadlines('DeathMessages/Enderman.txt'),
-            loadlines('DeathMessages/Fall.txt'),
-            loadlines('DeathMessages/Lava.txt'),
-            loadlines('DeathMessages/Llama.txt'),
-            loadlines('DeathMessages/Shulker.txt'),
-            loadlines('DeathMessages/Spider.txt'),
-            loadlines('DeathMessages/Suffocate.txt'),
-            loadlines('DeathMessages/TNT.txt'),
-            loadlines('DeathMessages/Void.txt'),
-            loadlines('DeathMessages/Witch_Magic.txt'),
-            loadlines('DeathMessages/Wolf.txt'),
-            loadlines('DeathMessages/Zombie.txt')
-        ]);
-})();
 
 module.exports = function set_up_discord_proxy(discord_client, bedrock_server, jason) {
     discord_client.on('message', message => {
@@ -47,18 +12,6 @@ module.exports = function set_up_discord_proxy(discord_client, bedrock_server, j
     jason.on('chat', (sender, message) => {
         if (sender !== 'JasonTheBot') {
             discord_client.channels.cache.get(channels['minecraft-chat']).send(`**${sender}**: ${message}`);
-        }
-    });
-    
-    jason.on('player-death', (player_dead, reason) => {
-        if (player_dead !== 'JasonTheBot') {
-            let message;
-            if (reason === '' || !death_messages.hasOwnProperty(reason)) {
-                message = 'died.';
-            } else {
-                message = death_messages[reason][Math.floor(Math.random() * death_messages[reason].length)];
-            }
-            discord_client.channels.cache.get(channels['minecraft-chat']).send(`${player_dead} ${message}`);
         }
     });
 }
