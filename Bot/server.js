6dc5e788c66d53c5ef532300e08c81f0d48e0ae7
Seth Brown <sl2004brown@gmail.com>
2020-09-12 20:29:33 -0600

    Have to force kill BDS

--- Bot/server.js
+++ Bot/server.js
@@ -45,7 +45,7 @@ module.exports = class bedrock_server extends EventEmitter {
             }
         } else {
             if (await this.BDS_running()) {
-                child_process.exec(`ssh ${this.#ssh_user}@${this.#server_ip} taskkill /IM "${path.basename(this.#program_path)}" /T`);
+                child_process.exec(`ssh ${this.#ssh_user}@${this.#server_ip} taskkill /F /T /IM "${path.basename(this.#program_path)}"`);
                 console.log('The BDS SSH handle that Node owns is dead, but BDS is running on the computer. Killing BDS');
             }
         }
