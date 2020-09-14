a77bc0186f8efdee401ae9c5e7704a7240f4e421
Seth Brown <sl2004brown@gmail.com>
2020-09-13 20:06:09 -0600

    Debugging death counter

--- Bot/death_counter.js
+++ Bot/death_counter.js
@@ -1,6 +1,8 @@
 module.exports = function set_up_death_counter(bedrock_server, jason) {
     jason.on('player-death', player_dead => {
         bedrock_server.write('scoreboard objectives add deathcount dummy Deaths');
+        console.log(`${player_dead} died`);
+        console.log(`execute ${player_dead} ~ ~ ~ scoreboard players add @s deathcount 1`);
         bedrock_server.write(`execute ${player_dead} ~ ~ ~ scoreboard players add @s deathcount 1`);
         bedrock_server.write('scoreboard objectives setdisplay list deathcount');
     });
