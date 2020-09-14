f13eeac5e006fec458f4d10fb84a07f960a26a07
Seth Brown <sl2004brown@gmail.com>
2020-09-13 20:00:45 -0600

    Correct deathcount?

--- Bot/death_counter.js
+++ Bot/death_counter.js
@@ -1,7 +1,7 @@
 module.exports = function set_up_death_counter(bedrock_server, jason) {
     jason.on('player-death', player_dead => {
         bedrock_server.write('scoreboard objectives add deathcount dummy Deaths');
-        bedrock_server.write(`scoreboard players add ${player_dead} deathcount 1`);
+        bedrock_server.write(`execute ${player_dead} ~ ~ ~ scoreboard players add @s deathcount 1`);
         bedrock_server.write('scoreboard objectives setdisplay list deathcount');
     });
 }
