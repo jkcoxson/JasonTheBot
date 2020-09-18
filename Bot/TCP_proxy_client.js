99d28929ac2e4a154efb5bf8ef45c7eec119fe52
Seth Brown <sl2004brown@gmail.com>
2020-09-18 16:30:44 -0600

    Make TCP pipe connect to the server, not localhost

--- Bot/TCP_proxy_client.js
+++ Bot/TCP_proxy_client.js
@@ -4,7 +4,7 @@ const process = require('process');
 const config = require('./configs/bedrock_server.config.json');
 
 const port = config.TCP_proxy_port;
-const conn = createConnection(port, 'localhost');
+const conn = createConnection(port, config.node_server_ip);
 conn.pipe(stdout);
 stdin.pipe(conn);
 conn.on('end', error => {
