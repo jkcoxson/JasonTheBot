48e0d8678d92cd89c90e13981dd5a75ac070c7b9
Seth Brown <sl2004brown@gmail.com>
2020-09-21 20:46:14 -0600

    Simplify death counter

--- Bot/death_counter.js
+++ Bot/death_counter.js
@@ -1,7 +1,7 @@
 module.exports = function set_up_death_counter(bedrock_server, jason) {
     jason.on('player-death', player_dead => {
         bedrock_server.write('scoreboard objectives add deathcount dummy Deaths');
-        bedrock_server.write(`execute @a[name=${player_dead}] ~ ~ ~ scoreboard players add @s deathcount 1`);
+        bedrock_server.write(`scoreboard players add @a[name=${player_dead}] deathcount 1`);
         bedrock_server.write('scoreboard objectives setdisplay list deathcount');
     });
 }
